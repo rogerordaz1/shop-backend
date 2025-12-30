f917ac9255211754800d52b216593a9a11da193f
Roger Ordaz <rogerordaz98@gmail.com>
2025-12-30 16:22:47 -0600

    added Mongo db conection configuration

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
+    require('dotenv').config();
+    const bodyParser = require('body-parser');
     const express = require('express');  
-    
-    
-    const app = express();
-    const PORT = process.env.PORT || 3000;
+    const morgan = require('morgan');
+    const cors = require('cors');
+    const mongoose = require('mongoose');
 
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
+
+    const app = express();
+
+    const PORT = process.env.PORT || 3000;
+    const HOSTNAME = process.env.HOSTNAME || 'localhost';
+    const MONGODB_URI = process.env.MONGODB_URI;
+
+    app.use(bodyParser.json());
+    app.use(morgan('tiny'));
+    app.use(cors());
+
+
+
+
+
+    
+    mongoose.connect(MONGODB_URI).then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
     });
 
 
-    
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+    // app.options('/*', cors());
+
+
+
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!!!!');
+    });
+
+
+
+    app.listen(PORT, HOSTNAME, () => {
+        console.log(`Server is running on port ${PORT} and hostname ${HOSTNAME}`);
     });
